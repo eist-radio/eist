e0ac89d212d814e5da0cf3a2bb072de718bcbf34
Aidan Reilly <oootini@yahoo.com>
2025-02-07 10:54:44 +0000

    Update copylink to not throw error when copy-link is not found

--- assets/js/copylink.js
+++ assets/js/copylink.js
@@ -1,24 +1,26 @@
-// Update the Artist page share button
+// Update the page share button
 const copyLink = document.getElementById('copy-link');
 
-// Add a click event listener to copy the URL to the clipboard
-copyLink.addEventListener('click', async (event) => {
-    event.preventDefault();
+if (copyLink) {
+    // Add a click event listener to copy the URL to the clipboard
+    copyLink.addEventListener('click', async (event) => {
+        event.preventDefault();
 
-    try {
-        // Copy the current page URL to the clipboard and update the text of the <a> element
-        await navigator.clipboard.writeText(window.location.href);
-        console.log('URL copied to clipboard:', window.location.href);
-        copyLink.innerHTML = 'Link copied! ';
+        try {
+            // Copy the current page URL to the clipboard and update the text of the <a> element
+            await navigator.clipboard.writeText(window.location.href);
+            console.log('URL copied to clipboard:', window.location.href);
+            copyLink.innerHTML = 'Link copied! ';
 
-        // Reset the text after 2 seconds
-        setTimeout(() => {
-            copyLink.innerHTML = 'Share this page ';
-        }, 2000);
-    } catch (error) {
-        console.error('Failed to copy URL:', error);
-    }
-});
+            // Reset the text after 2 seconds
+            setTimeout(() => {
+                copyLink.innerHTML = 'Share this page ';
+            }, 2000);
+        } catch (error) {
+            console.error('Failed to copy URL:', error);
+        }
+    });
 
-// Set the href attribute of the <a> element
-copyLink.href = window.location.href;
+    // Set the href attribute of the <a> element
+    copyLink.href = window.location.href;
+}
